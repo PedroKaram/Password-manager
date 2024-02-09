83f923b622674c4e1d757f2ce0a8f3d9dcc42960
Pedro Karam Nöcker <pedrokaram4005@gmail.com>
2024-02-09 19:57:16 -0300

    Setting up a basic UI and creating the password generator function

--- app/src/main/java/com/example/passwordmanager/MainActivity.kt
+++ app/src/main/java/com/example/passwordmanager/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.passwordmanager
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.passwordmanager.databinding.ActivityMainBinding
+import com.example.passwordmanager.services.Services
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    val Services = Services()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonGenerate.setOnClickListener {
+            binding.textViewGeneratedPassword.setText(Services.generatePassoword())
+        }
     }
 }
